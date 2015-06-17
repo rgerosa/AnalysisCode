ae3901888c7b3aabf1b2931cd5df02c75ae9e0c1
Adish Vartak <adishvartak@gmail.com>
2015-06-17 13:41:35 +0200

    Adding code to make the tree for the PDF/QCD scale weights

---
+++ MonoXAnalysis/AnalysisStep/plugins/LHEWeightsTreeMaker.cc
@@ -0,0 +1,148 @@
+#include <memory>
+#include <vector>
+#include <map>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h" 
+#include "SimDataFormats/GeneratorProducts/interface/LHEEventProduct.h"
+
+#include <TTree.h>
+
+class LHEWeightsTreeMaker : public edm::EDAnalyzer {
+    public:
+        explicit LHEWeightsTreeMaker(const edm::ParameterSet&);
+        ~LHEWeightsTreeMaker();
+        
+        static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+    
+    
+    private:
+        virtual void beginJob() override;
+        virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+        virtual void endJob() override;
+        
+        virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+        virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+        virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+        virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+        // InputTags
+        edm::InputTag lheInfoTag;
+
+        // Tokens
+        edm::EDGetTokenT<LHEEventProduct> lheInfoToken;
+
+        uint32_t event, run, lumi;
+        double   wgtxsec, wgtpdf1, wgtpdf2, wgtpdf3;
+        double*  wgtpdf;
+        double*  wgtqcd;
+        TTree* tree;
+};
+
+LHEWeightsTreeMaker::LHEWeightsTreeMaker(const edm::ParameterSet& iConfig): 
+    lheInfoTag(iConfig.getParameter<edm::InputTag>("lheinfo"))
+{
+    // Token consumes instructions
+    lheInfoToken = consumes<LHEEventProduct>(lheInfoTag);
+}
+
+
+LHEWeightsTreeMaker::~LHEWeightsTreeMaker() {
+}
+
+void LHEWeightsTreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+    using namespace edm;
+    using namespace std;
+
+    // Get handles to all the requisite collections
+    Handle<LHEEventProduct> lheInfoH;
+    iEvent.getByToken(lheInfoToken, lheInfoH);
+
+    // Event, lumi, run info
+    event = iEvent.id().event();
+    run   = iEvent.id().run();
+    lumi  = iEvent.luminosityBlock();
+
+    // Weights info
+    wgtxsec = 0.0;
+    wgtpdf1 = 0.0;
+    wgtpdf2 = 0.0;
+    wgtpdf3 = 0.0;
+    
+    wgtqcd = new double[8];
+    wgtpdf = new double[100];
+    for (size_t i = 0; i < 8  ; i++) wgtqcd[i] = 0.;
+    for (size_t i = 0; i < 100; i++) wgtpdf[i] = 0.;
+
+    vector<gen::WeightsInfo> weights = lheInfoH->weights();
+    for (size_t i = 0; i < weights.size(); i++) {
+        if (weights[i].id == "1"  ) wgtxsec = weights[i].wgt;
+        if (weights[i].id == "315") wgtpdf1 = weights[i].wgt;
+        if (weights[i].id == "316") wgtpdf2 = weights[i].wgt;
+        if (weights[i].id == "370") wgtpdf3 = weights[i].wgt;
+
+        for (size_t j = 2; j <= 9; j++) {
+            stringstream ss;
+            ss << j;
+            if (weights[i].id == ss.str()) wgtqcd[j-2]  = weights[i].wgt;
+        }
+        for (size_t j = 11; j <= 110; j++) {
+            stringstream ss;
+            ss << j;
+            if (weights[i].id == ss.str()) wgtpdf[j-11] = weights[i].wgt;
+        }
+    }
+
+    tree->Fill();
+}
+
+
+void LHEWeightsTreeMaker::beginJob() {
+    edm::Service<TFileService> fs;
+    tree = fs->make<TTree>("gentree"    , "gentree");
+    // Run, Lumi, Event info
+    tree->Branch("event"                , &event                , "event/i");
+    tree->Branch("run"                  , &run                  , "run/i");
+    tree->Branch("lumi"                 , &lumi                 , "lumi/i");
+    // Event weights
+    tree->Branch("wgtxsec"              , &wgtxsec              , "wgtxsec/D");
+    tree->Branch("wgtpdf1"              , &wgtpdf1              , "wgtpdf1/D");
+    tree->Branch("wgtpdf2"              , &wgtpdf2              , "wgtpdf2/D");
+    tree->Branch("wgtpdf3"              , &wgtpdf3              , "wgtpdf3/D");
+    tree->Branch("wgtpdf"               , &wgtpdf               , "wgtpdf[100]/D");
+    tree->Branch("wgtqcd"               , &wgtqcd               , "wgtqcd[8]/D");
+}
+
+void LHEWeightsTreeMaker::endJob() {
+}
+
+void LHEWeightsTreeMaker::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
+}
+
+void LHEWeightsTreeMaker::endRun(edm::Run const&, edm::EventSetup const&) {
+}
+
+void LHEWeightsTreeMaker::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
+}
+
+void LHEWeightsTreeMaker::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
+}
+
+void LHEWeightsTreeMaker::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+    edm::ParameterSetDescription desc;
+    desc.setUnknown();
+    descriptions.addDefault(desc);
+}
+
+DEFINE_FWK_MODULE(LHEWeightsTreeMaker);
+
