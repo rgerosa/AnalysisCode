8b911e29c5569ea1b20fbdddaffc832ed9d4719e
Adish Vartak <adishvartak@gmail.com>
2015-09-23 12:58:59 +0200

    Updates

--- MonoXAnalysis/AnalysisStep/test/crabConfig.py
+++ MonoXAnalysis/AnalysisStep/test/crabConfig.py
@@ -1,23 +1,12 @@
-from WMCore.Configuration import Configuration
-config = Configuration()
+from CRABClient.UserUtilities import config
+config = config()
 
-config.section_("General")
-config.General.requestName = 'MonoJet_dmAVM10'
-
-config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'tree.py'
-config.JobType.inputFiles = ['Summer15_50nsV2_MC.db']
 
-config.section_("Data")
-config.Data.inputDataset = '/DarkMatter_Monojet_M-10_AV_Tune4C_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 2
-config.Data.publication = False
-config.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'Monojet_dmAVM10'
+config.Data.inputDataset = '/QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 50000
 
-config.section_("Site")
 config.Site.storageSite = 'T2_US_UCSD'
 
