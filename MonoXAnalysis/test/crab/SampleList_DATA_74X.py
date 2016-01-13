ac87d8fb90295605ba8554aa5ca0afbfde33125b
Raffaele <raffaele.gerosa@cern.ch>
2016-01-13 19:09:36 +0100

    cross section fix

--- MonoXAnalysis/test/crab/SampleList_DATA_74X.py
+++
@@ -1,2 +0,0 @@
-samples['MET2015D_PromptReco_v3']  = ['/MET/Run2015D-PromptReco-v3/MINIAOD'
-                                      ['useLHEWeights=False','addQCDPDFWeights=False','isWorZMCSample=False']]   ## further options
--- MonoXAnalysis/test/crab/SampleList_MC_74X.py
+++
@@ -1,6 +0,0 @@
-#samples['ZJetsToNuNu_HT-100To200']  = ['/ZJetsToNuNu_HT-100To200_13TeV-madgraph/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM', ##dataset name
-#                                       ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True']]   ## further options
-#samples['ZJetsToNuNu_HT-200To400']  = ['/ZJetsToNuNu_HT-200To400_13TeV-madgraph/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM', ##dataset name
-#                                       ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True']]   ## further options
-samples['DYJetsToLL_M-50_HT-200to400']  = ['/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
-                                           ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True']]   ## further options
