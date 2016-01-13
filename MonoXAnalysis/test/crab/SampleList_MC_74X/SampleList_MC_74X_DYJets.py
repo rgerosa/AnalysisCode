7facc5127687e49a44af0410801805fb3aef0118
Raffaele <raffaele.gerosa@cern.ch>
2016-01-13 13:49:20 +0100

    updated MC list 74X

---
+++ MonoXAnalysis/test/crab/SampleList_MC_74X/SampleList_MC_74X_DYJets.py
@@ -0,0 +1,14 @@
+##DYJets LL samples LO                                                                                                                                                           
+samples['DYJetsToLL_M-50_HT-100To200']  = ['/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                           ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=148']]
+samples['DYJetsToLL_M-50_HT-200To400']  = ['/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                           ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=40.94']]
+samples['DYJetsToLL_M-50_HT-400To600']  = ['/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                           ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=5.497']]
+samples['DYJetsToLL_M-50_HT-600ToInf']  = ['/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                           ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=2.193']]
+
+# inclusive NLO
+samples['DYJetsToLL_M-50_amcatnlo'] = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                       ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=6025.2']]
+
---
+++ MonoXAnalysis/test/crab/SampleList_MC_74X/SampleList_MC_74X_PhotonJets.py
@@ -0,0 +1,9 @@
+## gamma+jets LO
+samples['GJets_HT-100To200'] = ['/GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=9235']]
+samples['GJets_HT-200To400'] = ['/GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=2298']]
+samples['GJets_HT-400To600'] = ['/GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=277.6']]
+samples['GJets_HT-600ToInf'] = ['/GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+                                ['useLHEWeights=True','addQCDPDFWeights=True','isWorZMCSample=True','crossSection=93.47']]
