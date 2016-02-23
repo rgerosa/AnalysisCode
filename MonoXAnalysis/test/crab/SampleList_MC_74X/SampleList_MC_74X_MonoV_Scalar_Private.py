1551d28f7a37cfc0e120f939c660a7f8b89da76f
Raffaele <raffaele.gerosa@cern.ch>
2016-02-23 09:36:58 +0100

    updates

---
+++ MonoXAnalysis/test/crab/SampleList_MC_74X/SampleList_MC_74X_MonoV_Scalar_Private.py
@@ -0,0 +1,4 @@
+samples['Higgs_ZH_125_10']   = ['/JHUGen_Higgs_ZH_125_10/dmytro-RunIISpring15DR74-1455908429-b67560f4b241138a80b43305410d39d2/USER',
+                                          ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=True','crossSection=-1']]
+samples['Higgs_WH_125_10']   = ['/JHUGen_Higgs_WH_125_10/dmytro-RunIISpring15DR74-1455908454-b67560f4b241138a80b43305410d39d2/USER',
+                                          ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=True','crossSection=-1']]
