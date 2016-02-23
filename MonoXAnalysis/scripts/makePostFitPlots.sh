2d77c3b5e9147baad5427a7c17d4a3e9cf0ac6df
Raffaele <raffaele.gerosa@cern.ch>
2016-02-23 09:40:04 +0100

    updates

--- MonoXAnalysis/scripts/makePostFitPlots.sh
+++ MonoXAnalysis/scripts/makePostFitPlots.sh
@@ -1,7 +1,7 @@
-root -l -b -q prepostZE.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1\);
-root -l -b -q prepostZM.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1\);
-root -l -b -q prepostWM.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1\);
-root -l -b -q prepostWE.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1\);
-root -l -b -q prepostGJ.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1\);
-root -l -b -q prepostSig.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1,\"Vector\",\"1000\",\"50\",false\);
-root -l -b -q prepostSig.C\(\"mlfit_monoj.root\",\"monoj_exclusive/templates_met.root\",\"met\",1,\"Vector\",\"1000\",\"50\",true\);
+root -l -b -q prepostZE.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostZM.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostWM.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostWE.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostGJ.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostSig.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2,\"Vector\",\"1000\",\"50\",false\);
+root -l -b -q prepostSig.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2,\"Vector\",\"1000\",\"50\",true\);
