eb8da0e53164763deac354753fa3bc123513adcf
Raffaele <raffaele.gerosa@cern.ch>
2016-03-08 18:49:48 +0100

    deleted not anymore 74X

--- MonoXAnalysis/test/crab/SampleList_DATA_74X/SampleList_DATA_74X_MET.py
+++
@@ -1,8 +0,0 @@
-samples['MET-Run2015C-05Oct2015-v1']   = ['/MET/Run2015C_25ns-05Oct2015-v1/MINIAOD',
-                                          ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
-
-samples['MET-Run2015D-05Oct2015-v1']   = ['/MET/Run2015D-05Oct2015-v1/MINIAOD',
-                                          ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
-
-samples['MET-Run2015D-PromptReco-v4']   = ['/MET/Run2015D-PromptReco-v4/MINIAOD',
-                                           ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
--- MonoXAnalysis/test/crab/SampleList_DATA_74X/SampleList_DATA_74X_SinglePhoton.py
+++
@@ -1,7 +0,0 @@
-samples['SinglePhoton-Run2015C-05Oct2015-v1']   = ['/SinglePhoton/Run2015C_25ns-05Oct2015-v1/MINIAOD',
-                                                   ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
-samples['SinglePhoton-Run2015D-05Oct2015-v1']   = ['/SinglePhoton/Run2015D-05Oct2015-v1/MINIAOD',
-                                                   ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
-
-samples['SinglePhoton-Run2015D-PromptReco-v4']   = ['/SinglePhoton/Run2015D-PromptReco-v4/MINIAOD',
-                                                   ['useLHEWeights=False','addQCDPDFWeights=False','isWorZorSignalMCSample=False','crossSection=-1']]
