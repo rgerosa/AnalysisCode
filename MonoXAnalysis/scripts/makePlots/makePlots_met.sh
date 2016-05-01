8f34d0839bc450db42a99a5e7459ad1cbffb954e
Raffaele <raffaele.gerosa@cern.ch>
2016-05-01 12:41:34 +0200

    updates for adding single and double hadronic tau CRs

---
+++ MonoXAnalysis/scripts/makePlots/makePlots_met.sh
@@ -0,0 +1,8 @@
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"zee\",false,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"zmm\",false,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"gam\",false,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"wen\",false,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"wmn\",false,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"topmu\",false,true,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"Recoil[GeV]\",\"topel\",false,true,true\)
+root -b -q -l ../makeControlPlots.C\(\"templates_observables.root\",1,\"met\",\"E_{T}^{miss}[GeV]\",\"SR\",false,true\)
---
+++ MonoXAnalysis/scripts/makePlots/makePostFitPlots.sh
@@ -0,0 +1,7 @@
+root -l -b -q prepostZE.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostZM.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostWM.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostWE.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostGJ.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2\);
+root -l -b -q prepostSig.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2,\"Vector\",\"1000\",\"50\",false\);
+root -l -b -q prepostSig.C\(\"mlfit_monoV.root\",\"monoV/templates_met.root\",\"met\",2,\"Vector\",\"1000\",\"50\",true\);
