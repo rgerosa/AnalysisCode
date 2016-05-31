ac374321b1325579bd1bac68676853da7553ab85
Raffaele <raffaele.gerosa@cern.ch>
2016-05-31 16:13:35 +0200

    updates

--- MonoXAnalysis/macros/makeControlPlotsFromTrees/showPlots.C
+++ MonoXAnalysis/macros/makeControlPlotsFromTrees/showPlots.C
@@ -4,24 +4,26 @@ void showPlots() {
 
   gROOT->SetBatch(kTRUE);
 
-  plot(Sample::zmm, "nvtx"         , ""       , "N_{PV}"        , "Events", "outputPlots","zmm_nvtx"  , 0.218, 25, 5.5, 30.5, 0.05, 1e5,true);
-  plot(Sample::zmm, "t1mumet"      , ""       , "Recoil [GeV]"        , "Events", "outputPlots","zmm_t1recoil"  , 0.218, 25, 0, 400, 0.05, 1e5,true);
-  plot(Sample::zmm, "t1pfmet"      , ""       , "Met [GeV]"           , "Events", "outputPlots","zmm_t1met"  ,    0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmet"        , ""       , "Raw Met [GeV]"       , "Events", "outputPlots","zmm_met",        0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmethadronHF", ""       , "Met hadron HF [GeV]" , "Events", "outputPlots","zmm_met_hadronHF"  ,   0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetegammaHF", ""       , "Met Egamma HF [GeV]"     , "Events", "outputPlots","zmm_met_egammaHF", 0.218, 20, 0, 50, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetchargedhadron", ""  , "Met Charge Hadron [GeV]" , "Events", "outputPlots","zmm_met_chargeHadron",  0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetneutralhadron", ""  , "Met Neutral Hadron [GeV]", "Events", "outputPlots","zmm_met_neutralHadron", 0.218, 25, 0, 120, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetelectrons", ""      , "Met Electrons [GeV]"     , "Events", "outputPlots","zmm_met_electrons", 0.218, 20, 0, 100, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetmuons"    , ""      , "Met Muons [GeV]"         , "Events", "outputPlots","zmm_met_muons",     0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zmm, "pfmetphotons"  , ""      , "Met Photons [GeV]"       , "Events", "outputPlots","zmm_met_photons",   0.218, 25, 0, 150, 0.05, 1e5,true);
+  plot(Sample::zmm, "zmass"        , ""       , "M_{#mu#mu} [GeV]"        , "Events", "outputPlots","zmm_zmass" , 0.59, 30, 60, 120, 0.05, 1e6,true);
+  plot(Sample::zee, "zeemass"        , ""       , "M_{ee} [GeV]"        , "Events", "outputPlots","zee_zmass" , 0.59, 30, 60, 120, 0.05, 1e6,true);
+  plot(Sample::zmm, "nvtx"         , ""       , "N_{PV}"        , "Events", "outputPlots","zmm_nvtx"  , 0.59, 25, 5.5, 30.5, 0.05, 1e6,true);
+  plot(Sample::zmm, "zpt"          , ""       , "N_{PV}"        , "Events", "outputPlots","zmm_pt"    , 0.59, 35, 0,  250, 0.05, 1e6,true);
+  plot(Sample::zmm, "t1mumet"      , ""       , "Recoil [GeV]"        , "Events", "outputPlots","zmm_t1recoil"  , 0.59, 25, 0, 400, 0.05, 1e6,true);
+  plot(Sample::zmm, "t1pfmet"      , ""       , "Met [GeV]"           , "Events", "outputPlots","zmm_t1met"  ,    0.59, 25, 0, 150, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmet"        , ""       , "Raw Met [GeV]"       , "Events", "outputPlots","zmm_met",        0.59, 25, 0, 150, 0.05, 1e5,true);  
+  plot(Sample::zmm, "pfmethadronHF", ""       , "Met hadron HF [GeV]" , "Events", "outputPlots","zmm_met_hadronHF"  ,   0.59, 25, 0, 150, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetegammaHF", ""       , "Met Egamma HF [GeV]"     , "Events", "outputPlots","zmm_met_egammaHF", 0.59, 20, 0, 50, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetchargedhadron", ""  , "Met Charge Hadron [GeV]" , "Events", "outputPlots","zmm_met_chargeHadron",  0.59, 25, 0, 150, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetneutralhadron", ""  , "Met Neutral Hadron [GeV]", "Events", "outputPlots","zmm_met_neutralHadron", 0.59, 25, 0, 120, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetelectrons", ""      , "Met Electrons [GeV]"     , "Events", "outputPlots","zmm_met_electrons", 0.59, 20, 0, 100, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetmuons"    , ""      , "Met Muons [GeV]"         , "Events", "outputPlots","zmm_met_muons",     0.59, 25, 0, 150, 0.05, 1e6,true);
+  plot(Sample::zmm, "pfmetphotons"  , ""      , "Met Photons [GeV]"       , "Events", "outputPlots","zmm_met_photons",   0.59, 25, 0, 150, 0.05, 1e6,true);
 
-  plot(Sample::zee, "nvtx"         , ""       , "N_{PV}"        , "Events", "outputPlots","zee_nvtx"  , 0.218, 25, 5.5, 30.5, 0.05, 1e5,true);
-  plot(Sample::zee, "t1elmet"      , ""       , "Recoil [GeV]"        , "Events", "outputPlots","zee_t1recoil"  , 0.218, 25, 0, 400, 0.05, 1e5,true);
-  plot(Sample::zee, "t1pfmet"      , ""       , "Met [GeV]"           , "Events", "outputPlots","zee_t1met"  ,    0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zee, "pfmet"        , ""       , "Raw Met [GeV]"       , "Events", "outputPlots","zee_met",        0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zee, "pfmethadronHF", ""       , "Met hadron HF [GeV]" , "Events", "outputPlots","zee_met_hadronHF"  ,   0.218, 25, 0, 150, 0.05, 1e5,true);
-  plot(Sample::zee, "pfmetegammaHF", ""       , "Met Egamma HF [GeV]"     , "Events", "outputPlots","zee_met_egammaHF", 0.218, 20, 0, 40, 0.05, 1e5,true);
+  /*
+    plot(Sample::zee, "t1pfmet"      , ""       , "Met [GeV]"           , "Events", "outputPlots","zee_t1met"  ,    0.218, 25, 0, 150, 0.05, 1e5,true);
+    plot(Sample::zee, "pfmet"        , ""       , "Raw Met [GeV]"       , "Events", "outputPlots","zee_met",        0.218, 25, 0, 150, 0.05, 1e5,true);
+    plot(Sample::zee, "pfmethadronHF", ""       , "Met hadron HF [GeV]" , "Events", "outputPlots","zee_met_hadronHF"  ,   0.218, 25, 0, 150, 0.05, 1e5,true);
+    plot(Sample::zee, "pfmetegammaHF", ""       , "Met Egamma HF [GeV]"     , "Events", "outputPlots","zee_met_egammaHF", 0.218, 20, 0, 40, 0.05, 1e5,true);
   plot(Sample::zee, "pfmetchargedhadron", ""  , "Met Charge Hadron [GeV]" , "Events", "outputPlots","zee_met_chargeHadron",  0.218, 25, 0, 150, 0.05, 1e5,true);
   plot(Sample::zee, "pfmetneutralhadron", ""  , "Met Neutral Hadron [GeV]", "Events", "outputPlots","zee_met_neutralHadron", 0.218, 25, 0, 120, 0.05, 1e5,true);
   plot(Sample::zee, "pfmetelectrons", ""      , "Met Electrons [GeV]"     , "Events", "outputPlots","zee_met_electrons", 0.218, 25, 0, 150, 0.05, 1e5,true);
@@ -76,6 +78,6 @@ void showPlots() {
   plot(Sample::topel, "pfmetelectrons", ""      , "Met Electrons [GeV]"     , "Events", "outputPlots","topel_met_electrons", 0.218, 20, 0, 100, 0.05, 1e6,true);
   plot(Sample::topel, "pfmetmuons"    , ""      , "Met Muons [GeV]"         , "Events", "outputPlots","topel_met_muons",     0.218, 20, 0, 100, 0.05, 1e6,true);
   plot(Sample::topel, "pfmetphotons"  , ""      , "Met Photons [GeV]"       , "Events", "outputPlots","topel_met_photons",   0.218, 25, 0, 150, 0.05, 1e6,true);
-
+  */
 
 }
