69d5483a87a85557aca773bfe0769a0c57183e01
Raffaele <raffaele.gerosa@cern.ch>
2016-10-10 17:38:30 +0200

    updates to speed up the code

--- MonoXAnalysis/macros/makeTriggerEfficiency/makeSinglePhotonTriggerEfficiency.C
+++ MonoXAnalysis/macros/makeTriggerEfficiency/makeSinglePhotonTriggerEfficiency.C
@@ -4,6 +4,9 @@
 #include "triggerUtils.h"
 #include "../CMS_lumi.h"
 
+vector<float> bins_singlePhoton = {160,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,180,190,200,210,220,235,250};
+vector<float> bins_jetHT = {175,180,190,200,250,300,350,400,450,500,550,600,650,700,800,1000,1200,1400};
+
 void makeSinglePhotonTriggerEfficiency(string inputDIR, string ouputDIR, float lumi = 0.86, bool useJetHT = false, bool drawFitFunctions =  false, int runCut = 999999999) {
 
   ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(1410065408);
@@ -17,36 +20,90 @@ void makeSinglePhotonTriggerEfficiency(string inputDIR, string ouputDIR, float l
   
   TF1 *fitfunc = new TF1("fitfunc", ErfCB, 150, 300, 5);
   fitfunc->SetParameters(165., 5., 5., 4., 1.);
-  vector<float> bins = {160,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,180,190,200,210,220,235,250};
+
+  TF1 *fitfunc_recover = new TF1("fitfunc_recover", ErfCB, 150, 300, 5);
+  fitfunc_recover->SetParameters(165., 5., 5., 4., 1.);
+
+  vector<float> bins;
   if(useJetHT)
-    bins = {175,180,190,200,250,300,350,400,450,500,550,600,650,700,800,1000,1200,1400};  
+    bins = bins_jetHT;
+  else
+    bins = bins_singlePhoton;  
   fitfunc->SetRange(bins.front(),bins.back());
   
   TChain* tree = new TChain("tree/tree");
   // should use the wmnu events triggered by single muon
   tree->Add((inputDIR+"/*root").c_str());
 
-  TH1F* hnum   = new TH1F("hnum", "", bins.size()-1, &bins[0]);
-  TH1F* hnum_2 = new TH1F("hnum_2", "", bins.size()-1, &bins[0]);
-  TH1F* hden   = new TH1F("hden", "", bins.size()-1, &bins[0]);
-  TH1F* hden_2 = new TH1F("hden_2", "", bins.size()-1, &bins[0]);
+  TH1F* hnum   = new TH1F("hnum", "",   bins.size()-1, &bins[0]);
+  TH1F* hden   = new TH1F("hden", "",   bins.size()-1, &bins[0]);
+  TH1F* hnum_recover = new TH1F("hnum_recover", "", bins.size()-1, &bins[0]);
+  TH1F* hden_recover = new TH1F("hden_recover", "", bins.size()-1, &bins[0]);
   hnum->Sumw2();
-  hnum_2->Sumw2();
   hden->Sumw2();
-  hden_2->Sumw2();
+  hnum_recover->Sumw2();
+  hden_recover->Sumw2();
 
-  if(not useJetHT){
-    // define numerator as event with a medium photon + trigger requirement
-    tree->Draw("phpt>>hnum",Form("nphotons == 1 && (hltphoton50 || hltphoton75 || hltphoton90 || hltphoton120) && (hltphoton175 || hltphoton165) && phidm == 1 && abs(pheta) < 1.4442 && run <= %d",runCut));    
-    // define denominator as an event with a tight muon passing single muon trigger
-    tree->Draw("phpt>>hden",Form("nphotons == 1 && phidm == 1 && abs(pheta) < 1.4442 && (hltphoton50 || hltphoton75 || hltphoton90 || hltphoton120) && run <= %d",runCut));    
-  }
-  else{
-    tree->Draw("phpt>>hnum",Form("nphotons == 1 && phidm == 1 && abs(pheta) < 1.4442 && (hltPFHT650 || hltPFHT400 || hltPFHT475 || hltPFHT600 || hltPFHT650) && (hltphoton175 || hltphoton165) && run <= %d",runCut));
-    tree->Draw("phpt>>hden",Form("nphotons == 1 && phidm == 1 && abs(pheta) < 1.4442 && (hltPFHT650 || hltPFHT400 || hltPFHT475 || hltPFHT600 || hltPFHT650) && run <= %d",runCut));
+  TTreeReader reader(tree);
+  TTreeReaderValue<UChar_t> hltp50    (reader,"hltphoton50");
+  TTreeReaderValue<UChar_t> hltp75    (reader,"hltphoton75");
+  TTreeReaderValue<UChar_t> hltp90    (reader,"hltphoton90");
+  TTreeReaderValue<UChar_t> hltp120   (reader,"hltphoton120");
+  TTreeReaderValue<UChar_t> hltp165   (reader,"hltphoton165");
+  TTreeReaderValue<UChar_t> hltp175   (reader,"hltphoton175");
 
-    tree->Draw("phpt>>hnum_2",Form("nphotons == 1 && phidm == 1 && abs(pheta) < 1.4442 && (hltPFHT650 || hltPFHT400 || hltPFHT475 || hltPFHT600 || hltPFHT650) && (hltphoton175 || hltphoton165 || hltPFHT800) && run <= %d",runCut));
-    tree->Draw("phpt>>hden_2",Form("nphotons == 1 && phidm == 1 && abs(pheta) < 1.4442 && (hltPFHT650 || hltPFHT400 || hltPFHT475 || hltPFHT600 || hltPFHT650) && run <= %d",runCut));
+  TTreeReaderValue<UChar_t> hltht400    (reader,"hltPFHT400");
+  TTreeReaderValue<UChar_t> hltht475    (reader,"hltPFHT475");
+  TTreeReaderValue<UChar_t> hltht600    (reader,"hltPFHT600");
+  TTreeReaderValue<UChar_t> hltht650    (reader,"hltPFHT650");
+  TTreeReaderValue<UChar_t> hltht800   (reader,"hltPFHT800");
+
+  TTreeReaderValue<double>  phpt     (reader,"phpt");
+  TTreeReaderValue<double>  pheta    (reader,"pheta");
+  TTreeReaderValue<int>     phidm    (reader,"phidm");
+
+  TTreeReaderValue<int>     nphotons (reader,"nphotons");
+  TTreeReaderValue<int>     nmuons   (reader,"nmuons");
+  TTreeReaderValue<int>     nelectrons (reader,"nelectrons");
+  TTreeReaderValue<int>     nbjets   (reader,"nbjetslowpt");
+  TTreeReaderValue<int>     ntaus    (reader,"ntausraw");
+
+  TTreeReaderValue<UChar_t> fcsc   (reader,"flagglobaltighthalo");
+  TTreeReaderValue<UChar_t> fcsct  (reader,"flagcsctight");
+  TTreeReaderValue<UChar_t> feeb   (reader,"flageebadsc");
+  TTreeReaderValue<UChar_t> fetp   (reader,"flagecaltp");
+  TTreeReaderValue<UChar_t> fvtx   (reader,"flaggoodvertices");
+  TTreeReaderValue<UChar_t> fbadmu (reader,"flagbadpfmu");
+  TTreeReaderValue<UChar_t> fbadch (reader,"flagbadchpf");
+
+  while(reader.Next()){
+
+    if(*nmuons != 0)   continue;
+    if(*nelectrons != 0) continue;
+    if(*nbjets != 0)   continue;
+    if(*ntaus != 0)    continue;
+    if(*nphotons == 1) continue;
+    if(fabs(*pheta) > 1.4442) continue;
+    if(*phidm != 1) continue;
+    if(not *fcsc or not *fcsct or not *feeb or not *fetp or not *fvtx or not *fbadmu or not *fbadch) continue;
+
+    if(not useJetHT){
+      if(*hltp50 or *hltp75 or *hltp90 or *hltp120){
+	hden->Fill(*phpt);
+	if(*hltp165 or *hltp175)
+	  hnum->Fill(*phpt);
+      }
+    }
+    else{
+      if(*hltht400 or *hltht475 or *hltht600 or *hltht650){
+	hden->Fill(*phpt);
+	hden_recover->Fill(*phpt);
+	if(*hltp165 or *hltp175)
+	  hnum->Fill(*phpt);
+	if(*hltp165 or *hltp175 or *hltht800)
+	  hnum_recover->Fill(*phpt);	  
+      }      	
+    }
   }
 
   TEfficiency* eff = new TEfficiency(*hnum,*hden);  
@@ -54,10 +111,11 @@ void makeSinglePhotonTriggerEfficiency(string inputDIR, string ouputDIR, float l
   eff->SetLineColor(kBlack);
   eff->SetMarkerStyle(20);
   eff->SetMarkerSize(1);
-  fitfunc->SetLineColor(kBlue);
+  fitfunc->SetLineColor(kBlack);
   fitfunc->SetLineWidth(2);
+
   
-  TH1* frame = canvas->DrawFrame(bins.front(), 0.4, bins.back(), 1.1, "");
+  TH1* frame = canvas->DrawFrame(bins.front(), 0.1, bins.back(), 1.1, "");
   frame->GetXaxis()->SetTitle("Photon p_{T} [GeV]");
   frame->GetYaxis()->SetTitle("Trigger Efficiency");
   frame->GetYaxis()->SetLabelSize(0.8*frame->GetYaxis()->GetLabelSize());
@@ -79,28 +137,22 @@ void makeSinglePhotonTriggerEfficiency(string inputDIR, string ouputDIR, float l
   canvas->RedrawAxis();
   CMS_lumi(canvas,string(Form("%.2f",lumi)),true);
 
-  
-  TEfficiency* eff_2 = NULL;
-  TF1* fitfunc_2 = NULL;
+  TEfficiency* eff_recover = NULL;
 
   if(useJetHT){
 
-    eff_2 = new TEfficiency(*hnum_2,*hden_2);  
-    fitfunc_2 = (TF1*) fitfunc->Clone("fitfunc_2");
-    fitfunc_2->SetParameters(165., 5., 5., 4., 1.);
-    eff->SetMarkerColor(kBlue);
-    eff_2->SetMarkerColor(kRed);
-    eff->SetLineColor(kBlue);
-    eff_2->SetLineColor(kRed);
-    eff_2->SetMarkerStyle(20);
-    eff_2->SetMarkerSize(1);
-    fitfunc_2->SetLineColor(kRed);
-    fitfunc_2->SetLineWidth(2);
+    eff_recover = new TEfficiency(*hnum_recover,*hden_recover);  
+    eff_recover->SetMarkerColor(kRed);
+    eff_recover->SetLineColor(kRed);
+    eff_recover->SetMarkerStyle(20);
+    eff_recover->SetMarkerSize(1);
+    fitfunc_recover->SetLineColor(kRed);
+    fitfunc_recover->SetLineWidth(2);
     
-    eff_2->Draw("E1PSAME");
+    eff_recover->Draw("E1PSAME");
     if(drawFitFunctions){
-      eff_2->Fit(fitfunc_2);
-      fitfunc_2->Draw("SAME");
+      eff_recover->Fit(fitfunc_recover);
+      fitfunc_recover->Draw("SAME");
     }
     canvas->RedrawAxis();
     
@@ -109,20 +161,21 @@ void makeSinglePhotonTriggerEfficiency(string inputDIR, string ouputDIR, float l
     leg->SetFillStyle(0);
     leg->SetBorderSize(0);
     leg->AddEntry(eff,"Photon 165/175","PLE");
-    leg->AddEntry(eff_2,"Photon || PFHT800 ","PLE");
+    leg->AddEntry(eff_recover,"Photon || PFHT800 ","PLE");
     leg->Draw("same");
   }
  
   canvas->SaveAs((ouputDIR+"/photonTriggerEff.png").c_str(),"png");
   canvas->SaveAs((ouputDIR+"/photonTriggerEff.pdf").c_str(),"pdf");
+
   TFile* outputFile = new TFile((ouputDIR+"/photonTriggerEfficiency.root").c_str(),"RECREATE");
   outputFile->cd();
   eff->Write("efficiency_photon");
-  if(eff_2 and eff_2 != NULL)
-    eff_2->Write("efficiency_photon_pfht");
+  if(eff_recover and eff_recover != NULL)
+    eff_recover->Write("efficiency_photon_recover");
   fitfunc->Write("efficiency_func");
-  if(fitfunc_2 and fitfunc_2 != NULL)
-    fitfunc_2->Write("efficiency_func_pfht");
-
+  if(fitfunc_recover and fitfunc_recover != NULL)
+    fitfunc_recover->Write("efficiency_func_recover");
+  
 }
 
