a9f60fe9764f03a9e62fab4f74f8b6a332bc7a3a
Raffaele <raffaele.gerosa@cern.ch>
2017-01-08 11:24:44 +0100

    deleted

--- MonoXAnalysis/scripts/metfilter.sh
+++
@@ -1,8 +0,0 @@
-#root -l -b -q macros/metfilter.C\(\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SinglePhoton/zeefilter/zee_tree_crab_SinglePhoton-Run2015.root\",\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SinglePhoton/zeefilter/zee_tree_crab_SinglePhoton-Run2015_new.root\"\);
-
-root -l -b -q macros/metfilter.C\(\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SinglePhoton/wenfilter/wen_tree_crab_SinglePhoton-Run2015.root\",\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SinglePhoton/wenfilter/wen_tree_crab_SinglePhoton-Run2015_new.root\"\);
-
-
-#root -l -b -q macros/metfilter.C\(\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SingleElectron/zeefilter/zee_tree_crab_SingleEle-Run2015.root\",\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SingleElectron/zeefilter/zee_tree_crab_SingleEle-Run2015_new.root\"\);
-
-root -l -b -q macros/metfilter.C\(\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SingleElectron/wenfilter/wen_tree_crab_SingleEle-Run2015.root\",\"/home/rgerosa/MONOJET_ANALYSIS/Production-19-2-2016/SingleElectron/wenfilter/wen_tree_crab_SingleEle-Run2015_new.root\"\);
