5f70387d0f956c0c09cffd6c1b25a689fd0375c6
Marco Cipriani <cippy91@gmail.com>
2017-01-19 00:03:09 +0100

    adding instructions to use this macro

--- MonoXAnalysis/macros/makePhotonPurity/runMakePhotonPurity.C
+++ MonoXAnalysis/macros/makePhotonPurity/runMakePhotonPurity.C
@@ -7,6 +7,14 @@
 
 using namespace std;
 
+// usage :
+// root -l -q -b 'runMakePhotonPurity.C++("argument1","argument2",...)' &> logfile.log  
+
+// &> logfile.log is useful if you want to save output in a log for debugging
+// You call this macro with the same arguments as makePhotonPurityFit.C, but using only strings (to build command in an easier way)
+// e.g., if you pass a number as the lumi or a bool as addSystematics, you write these as if they were strings
+
+
 void runMakePhotonPurity(const string& inputDirectory = "", // directory with dataFiles (file names must match with RunEra: all matched files, and only them, are used)    
                          const string& lumi = "36.2", // luminosity                                                                      
                          const string& outputDIR = "",
