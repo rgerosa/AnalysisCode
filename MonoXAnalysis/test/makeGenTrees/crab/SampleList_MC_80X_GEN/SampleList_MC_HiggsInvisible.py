88253b23f83c2d449556f12dda80772d7a8a1284
Raffaele <raffaele.gerosa@cern.ch>
2017-06-03 18:12:19 +0200

    updates

---
+++ MonoXAnalysis/test/makeGenTrees/crab/SampleList_MC_80X_GEN/SampleList_MC_HiggsInvisible.py
@@ -0,0 +1,5 @@
+######### VBF
+samples['VBF_HToInvisible_M125'] = ['/VBF_HToInvisible_M125_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',['crossSection=3.782','isSignalSample=True','minBosonPt=0']]
+
+######## GGH
+samples['GluGlu_HToInvisible_M125'] = ['/GluGlu_HToInvisible_M125_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',['crossSection=4.520e+01','isSignalSample=True','minBosonPt=0']]
